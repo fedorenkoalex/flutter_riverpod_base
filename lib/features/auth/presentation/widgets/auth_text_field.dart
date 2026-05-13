e67161854125b049c2b746e2f36b6b0285cd7a08
oleksandr <alexandr.fedorenko@onix-systems.com>
2026-05-13 17:38:51 +0300

    refactor: make obscureText a non-nullable boolean in AuthTextField

--- lib/features/auth/presentation/widgets/auth_text_field.dart
+++ lib/features/auth/presentation/widgets/auth_text_field.dart
@@ -6,9 +6,9 @@ class AuthTextField extends StatelessWidget{
   final TextEditingController controller;
   final String label;
   final IconData icon;
-  final bool? obscureText;
+  final bool obscureText;
 
-  const AuthTextField({super.key, required this.controller, required this.label, required this.icon, this.obscureText = false})
+  const AuthTextField({super.key, required this.controller, required this.label, required this.icon, this.obscureText = false});
   @override
   Widget build(BuildContext context) {
     return TextField(
