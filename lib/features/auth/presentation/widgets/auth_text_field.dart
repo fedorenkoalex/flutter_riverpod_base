bdfed6c48fa83bdf480aebe41caa160b80a189a1
oleksandr <alexandr.fedorenko@onix-systems.com>
2026-05-13 17:39:23 +0300

    style: format AuthTextField code and add missing semicolon

--- lib/features/auth/presentation/widgets/auth_text_field.dart
+++ lib/features/auth/presentation/widgets/auth_text_field.dart
@@ -1,14 +1,18 @@
-
-
 import 'package:flutter/material.dart';
 
-class AuthTextField extends StatelessWidget{
+class AuthTextField extends StatelessWidget {
   final TextEditingController controller;
   final String label;
   final IconData icon;
   final bool obscureText;
 
-  const AuthTextField({super.key, required this.controller, required this.label, required this.icon, this.obscureText = false});
+  const AuthTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    required this.icon,
+    this.obscureText = false,
+  });
   @override
   Widget build(BuildContext context) {
     return TextField(
@@ -30,6 +34,6 @@ class AuthTextField extends StatelessWidget{
         filled: true,
         fillColor: Colors.white.withValues(alpha: 0.1),
       ),
-    )
+    );
   }
 }
